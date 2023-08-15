16eb7a124ed68f73748569d1583da7da42280e9f
Cheppar <ejioforched@gmail.com>
2023-08-15 15:28:02 +0300

    Initial Commit

---
+++ Card/CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardApp.swift
+//  Card
+//
+//  Created by Mac User on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
